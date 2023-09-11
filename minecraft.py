0a96d2bd9520f533f4bf36e586ae27ec0d136dde
Ryan William O'Hara <ryan@ryanwohara.com>
2023-09-11 16:17:12 -0600

    feat: !fortune

--- minecraft.py
+++ minecraft.py
@@ -1,6 +1,7 @@
 import threading
 import time
 from subprocess import Popen, PIPE
+import random
 import re
 
 
@@ -39,10 +40,13 @@ class Minecraft():
                     r"(?:\[[^]]+\] \[Server thread/INFO\]|\[\d+:\d+:\d+ INFO]): (\S+) left the game", output)
 
                 advancement = re.match(
-                    r"(?:\[[^]]+\] \[Server thread/INFO\]|\[\d+:\d+:\d+ INFO]): (\S+ has (made the advancement|completed the challenge|has reached the goal).*)", output)
+                    r"(?:\[[^]]+\] \[Server thread/INFO\]|\[\d+:\d+:\d+ INFO]): ((\S+ has (?:made the advancement|completed the challenge|has reached the goal)).*)", output)
 
                 privmsg = re.match(
                     r"(?:\[[^]]+\] \[Server thread/INFO\]|\[\d+:\d+:\d+ INFO]): (?:\[Not Secure\])? ?(?!<Server>)(<[^>]+> (.*)|\* (.*)|[^:\[\]*/]+)$", output)
+
+                objective = re.match(
+                    r"(?:\[[^]]+\] \[Server thread/INFO\]|\[\d+:\d+:\d+ INFO]): \S+\[@: Created new objective \[([^]]+)\]\]", output)
 
                 if join:
                     nick = join.group(1)
@@ -59,11 +63,25 @@ class Minecraft():
                         "#minecraft", "<-- " + nick)
                     self.players.remove(nick)
                 elif advancement:
+                    adv = advancement.group(1)
+                    if ';' in adv:
+                        new_advancement = re.match(r".+(\[[^]]+\]).+", adv)
+                        adv = ' '.join(advancement.group(2), new_advancement.group(1))
                     self.irc.privmsg(
-                        "#minecraft", advancement.group(1))
+                        "#minecraft", adv)
                 elif privmsg:
-                    if not privmsg.group(1).startswith('UUID of player '):
+                    if not privmsg.group(1).startswith('UUID of player ') and not (privmsg.group(1).startswith('Player profile key for ') and privmsg.group(1).endswith(' has expired!')):
                         self.irc.privmsg("#minecraft", privmsg.group(1))
+                elif objective:
+                    cmd = objective.group(1)
+
+                    if cmd == "!fortune":
+                        fortune = self.fortune()
+                        while len(fortune) > 150:
+                            fortune = self.fortune()
+
+                        self.privmsg(fortune.replace("\n", " ").replace("\r", ""))
+
 
     def rawInput(self):
         while True:
@@ -82,3 +100,8 @@ class Minecraft():
 
     def get_players(self):
         return self.players
+
+    def fortune(self):
+        with open('/usr/share/games/fortunes/fortunes') as f:
+            fortunes = f.readlines()
+            return random.choice(''.join(fortunes).split('%'))
